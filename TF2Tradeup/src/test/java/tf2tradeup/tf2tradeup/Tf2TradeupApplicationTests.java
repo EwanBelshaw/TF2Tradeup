c6d84149829d71bd3c7b51410228f541e90b6771
Ewan Belshaw <ewanbelshaw@gmail.com>
2025-05-10 17:25:37 -0400

    Initialized springboot project

---
+++ TF2Tradeup/src/test/java/tf2tradeup/tf2tradeup/Tf2TradeupApplicationTests.java
@@ -0,0 +1,13 @@
+package tf2tradeup.tf2tradeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tf2TradeupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
