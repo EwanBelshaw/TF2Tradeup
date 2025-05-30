da797b9a1e63148c15cdaed0ce4fb70b2eb8bb4a
Ewan Belshaw <ewanbelshaw@gmail.com>
2025-05-30 19:54:58 -0400

    Created some interfaces and enums for blogs.

--- Frontend/src/domain/domain.ts
+++ Frontend/src/domain/domain.ts
@@ -10,3 +10,20 @@ export const isErrorResponse = (obj: any): obj is ErrorResponse => {
     typeof obj.error === "string"
   );
 };
+
+export enum BlogTag{
+    WARPAINT="WARPAINT",
+    HAT="HAT",
+    TRADING="TRADING",
+    TRADEUP="TRADEUP",
+    PATTERN="PATTERN"
+}
+
+export interface CreateBlogRequest {
+    title: string,
+    description?: string,
+    content: string,
+    createdAt: Date,
+    tag?: BlogTag,
+   
+}
