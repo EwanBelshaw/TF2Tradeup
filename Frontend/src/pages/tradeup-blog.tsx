d7a9a3cf5008d30614ad91adae395c28129671ac
Ewan Belshaw <ewanbelshaw@gmail.com>
2025-06-06 11:16:59 -0400

    Blog form partially created.

--- Frontend/src/pages/tradeup-blog.tsx
+++ Frontend/src/pages/tradeup-blog.tsx
@@ -1,15 +1,27 @@
+import { Textarea } from "@/components/ui/textarea";
 import NavBar from "../components/nav-bar";
 import { Input } from "../components/ui/input";
 import { useState } from "react";
 
 interface BlogData {
   title: string;
+  description: string;
+  content: string;
 }
 
 const Blog: React.FC = () => {
   const [blogData, setBlogData] = useState<BlogData>({
     title: "",
+    description: "",
+    content: "",
   });
+
+
+  const updateField = (field: keyof BlogData,
+    value: any
+  ) => {
+    setBlogData((prev) => ({...prev, [field]: value}))
+  }
 
   const formSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -19,15 +31,30 @@ const Blog: React.FC = () => {
     <div className="min-h-screen bg-orange-300 text-white">
       <NavBar />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold">Create a blog!</h1>
+        <h1 className="text-2xl font-bold mb-6">Create a blog!</h1>
         <form onSubmit={formSubmit}>
           <div>
             <Input
-              id="tradeup-name"
-              className="bg-white border-black text-black"
-              placeholder="Tradeup name"
+              id="blog-title"
+              className="bg-white border-black text-black mb-6"
+              placeholder="Blog title"
+              onChange={(e) => updateField("title", e.target.value)}
+              required
             />
+             <Input
+              id="blog-description"
+              className="bg-white border-black text-black mb-6"
+              placeholder="Blog description"
+              onChange={(e) => updateField("description", e.target.value)}
+              required
+            />
+            <Textarea
+            className="bg-white border-black text-black mb-6"
+            placeholder="Blog content"
+            onChange={(e) => updateField("content", e.target.value)}
+            required/>
           </div>
+          <p>{JSON.stringify(blogData)}</p>
         </form>
       </div>
     </div>
