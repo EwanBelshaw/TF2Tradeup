095fa13d4f89055407fc04e70344b36eb2d11018
Ewan Belshaw <ewanbelshaw@gmail.com>
2025-06-17 14:35:11 -0400

    Dto's

--- Backend/src/main/java/com/TF2Tradeup/Backend/mappers/BlogMapper.java
+++ Backend/src/main/java/com/TF2Tradeup/Backend/mappers/BlogMapper.java
@@ -1,5 +1,7 @@
 package com.TF2Tradeup.Backend.mappers;
 
+import com.TF2Tradeup.Backend.dtos.CreateBlogResponseDto;
+import com.TF2Tradeup.Backend.entities.blog.Blog;
 import com.TF2Tradeup.Backend.requests.CreateBlogRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -7,4 +9,5 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface BlogMapper {
     CreateBlogRequest fromDto(CreateBlogRequest createBlogRequest);
+    CreateBlogResponseDto toDto(Blog blog);
 }
